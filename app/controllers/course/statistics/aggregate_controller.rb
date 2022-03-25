a8baab5463156ba834fac033801bf683d7af5c66
Hanming Zhu <hanming@huawen.com>
2022-03-25 14:42:54 +0800

    feat: add some preliminary student performance data to aggregate controller

--- app/controllers/course/statistics/aggregate_controller.rb
+++ app/controllers/course/statistics/aggregate_controller.rb
@@ -2,25 +2,12 @@
 # This is named aggregate controller as naming this as course controller leads to name conflict issues
 class Course::Statistics::AggregateController < Course::Statistics::Controller
   def course
-    @assessments = Course::Assessment.joins(lesson_plan_item: :default_reference_time).
-                   where(course_id: current_course.id).
-                   where.not(course_reference_times: { end_at: nil }).
-                   pluck(:id, :title, :start_at, :end_at).
-                   to_a
-    user_submission_date_pairs = Course::Assessment::Submission.joins(creator: :course_users).
-                                 where(assessment_id: @assessments.map { |i| i[0] }).
-                                 where(course_users: { course_id: current_course.id }).
-                                 where(course_users: { role: :student }).
-                                 group(:creator_id, 'course_users.name').
-                                 pluck(:creator_id, 'course_users.name', 'json_agg(assessment_id)', 'array_agg(submitted_at)').
-                                 to_a
-    @submissions = user_submission_date_pairs.map do |pair|
-      data = pair[2].zip(pair[3]).filter { |_, submitted_at| !submitted_at.nil? }.
-             map do |assessment_id, submitted_at|
-        [assessment_id, submitted_at]
-      end
-      [pair[0], pair[1], data]
-    end
+    @assessment_info_array = assessment_info_array
+    @user_submission_array = user_submission_array
+    num_students = current_course.course_users.students.count
+    num_to_query = [1, num_students / 20].max
+    @students_by_learning_rate = students_by_learning_rate(num_to_query)
+    @students_by_num_submissions = students_by_number_of_submissions(num_to_query)
   end
 
   def all_staff
@@ -35,6 +22,110 @@ class Course::Statistics::AggregateController < Course::Statistics::Controller
   end
 
   private
+
+  def assessment_info_array
+    @assessment_info_array ||= Course::Assessment.joins(lesson_plan_item: :default_reference_time).
+                               where(course_id: current_course.id).
+                               where.not(course_reference_times: { end_at: nil }).
+                               pluck(:id, :title, :start_at, :end_at).
+                               to_a
+  end
+
+  def user_submission_array
+    @user_submission_array ||= Course::Assessment::Submission.joins(creator: :course_users).
+                               where(assessment_id: assessment_info_array.map do |i|
+                                                      i[0]
+                                                    end, course_users: { course_id: current_course.id,
+                                                                         role: :student }).
+                               group(:creator_id, 'course_users.name').
+                               pluck(:creator_id, 'course_users.name', 'json_agg(assessment_id)',
+                                     'array_agg(submitted_at)').
+                               to_a.map do |pair|
+      data = pair[2].zip(pair[3]).filter { |_, submitted_at| !submitted_at.nil? }.
+             map do |assessment_id, submitted_at|
+        [assessment_id, submitted_at]
+      end
+      [pair[0], pair[1], data]
+    end
+  end
+
+  def students_by_number_of_submissions(num_to_query)
+    query = current_course.course_users.students.
+            joins('INNER JOIN course_assessment_submissions
+      ON (course_users.user_id = course_assessment_submissions.creator_id)').
+            where(course_assessment_submissions: { workflow_state: [:submitted, :graded, :published] }).
+            group('course_users.id', :name)
+    [query.order('count(course_assessment_submissions.id)').limit(num_to_query).
+      pluck('course_users.id', :name, 'count(course_assessment_submissions.id)'),
+     query.order('count(course_assessment_submissions.id) desc').limit(num_to_query).
+       pluck('course_users.id', :name, 'count(course_assessment_submissions.id)')]
+  end
+
+  def students_by_correctness(num_to_query)
+    query = CourseUser.find_by_sql(<<-SQL.squish
+      SELECT
+        id,
+        name,
+        AVG(correctness) AS correctness
+      FROM (
+        SELECT
+          cu.id AS id,
+          cu.name AS name,
+          cas.id AS sub_id,
+          sum(caa.grade) / sum(caq.maximum_grade) AS correctness
+        FROM
+          course_users cu
+          INNER JOIN course_assessment_submissions cas
+          ON (cu.user_id = cas.creator_id)
+          INNER JOIN course_assessments ca
+          ON (cas.assessment_id = ca.id)
+          INNER JOIN course_assessment_answers caa
+          ON (caa.submission_id = cas.id)
+          INNER JOIN course_question_assessments cqa
+          ON (cqa.assessment_id = ca.id)
+          INNER JOIN course_assessment_questions caq
+          ON (cqa.question_id = caq.id)
+        WHERE
+          cu.course_id = #{current_course.id}
+          AND cu.role = 0
+          AND ((ca.autograded = true AND cas.workflow_state <> 'attempting') OR
+            (ca.autograded = false AND cas.workflow_state IN ('graded', 'published')))
+        GROUP BY
+          cu.id,
+          cu.name,
+          cas.id
+        HAVING
+          sum(caq.maximum_grade) > 0
+      ) course_user_assessment_correctness
+      GROUP BY
+        id,
+        name
+      ORDER BY
+        correctness
+      LIMIT
+        #{num_to_query}
+    SQL
+                                  )
+    [query, []]
+  end
+
+  def students_by_lateness
+    [[], []]
+  end
+
+  # Returns a [[worst 5% students], [best 5% students]] based on learning rate.
+  def students_by_learning_rate(num_to_query)
+    return [[], []] unless current_course.show_personalized_timeline_features?
+
+    query = current_course.course_users.students.
+            joins(:learning_rate_records).
+            where(course_learning_rate_records: { most_recent: true })
+
+    # The higher the learning rate, the worse
+    [query.order(learning_rate: :desc).limit(num_to_query).pluck(:id, :name,
+                                                                 'course_learning_rate_records.learning_rate'),
+     query.order(:learning_rate).limit(num_to_query).pluck(:id, :name, 'course_learning_rate_records.learning_rate')]
+  end
 
   def course_users
     @course_users ||= current_course.course_users.includes(:groups)
--- app/views/course/statistics/aggregate/course.json.jbuilder
+++ app/views/course/statistics/aggregate/course.json.jbuilder
@@ -1,16 +1,27 @@
 # frozen_string_literal: true
-json.assessments @assessments do |assessment|
+json.assessments @assessment_info_array do |assessment|
   json.id assessment[0]
   json.title assessment[1]
   json.startAt assessment[2].iso8601
   json.endAt assessment[3]&.iso8601
 end
 
-json.submissions @submissions do |user|
+json.submissions @user_submission_array do |user|
   json.id user[0]
   json.name user[1]
   json.submissions user[2] do |submission|
     json.assessmentId submission[0]
     json.submittedAt submission[1].iso8601
+  end
+end
+
+json.students do
+  json.learningRate do
+    json.worst @students_by_learning_rate[0]
+    json.best @students_by_learning_rate[1]
+  end
+  json.numSubmissions do
+    json.worst @students_by_num_submissions[0]
+    json.best @students_by_num_submissions[1]
   end
 end
